fbfb68edda175fffd20a85762cb44158b4e5b3fe
kota-shiokara <kota.book.39@gmail.com>
2022-10-01 00:15:26 +0900

    Mapの表示だけやった、現在地とかは後ほど

---
+++ app/src/main/java/fun/hackathon/hima/data/model/Params.kt
@@ -0,0 +1,7 @@
+package `fun`.hackathon.hima.data.model
+
+class Params {
+    companion object {
+        val REQUEST_CODE_LOCATION = 100
+    }
+}
--- app/src/main/java/fun/hackathon/hima/ui/pages/MainScreen.kt
+++ app/src/main/java/fun/hackathon/hima/ui/pages/MainScreen.kt
@@ -1,26 +1,31 @@
 package `fun`.hackathon.hima.ui.pages
 
-import `fun`.hackathon.hima.Greeting
 import `fun`.hackathon.hima.LocalNavController
 import `fun`.hackathon.hima.R
+import `fun`.hackathon.hima.data.model.Params
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.maps.android.compose.GoogleMap
 
 @Composable
 fun MainScreen() {
     val navController = LocalNavController.current
-    
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -39,6 +44,31 @@ fun MainScreen() {
             }
         }
     ) {
-        
+        // MainContent()
+    }
+}
+
+@Composable
+fun MainContent() {
+    val context = LocalContext.current
+
+    if (ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED) {
+
+        GoogleMap(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+        }
+    } else {
+        // 許可ダイアログ出しといた方が良さそう
+        // 許可求める
+        ActivityCompat.requestPermissions(
+            context as Activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            Params.REQUEST_CODE_LOCATION
+        )
     }
 }
